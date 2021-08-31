11b5a0b484c2f484041bb4c011e75bdd6bd897d8
Paul Kremer <p.kremer@uni.lu>
2021-08-31 14:27:48 +0200

    firefly hil

--- scripts/build_firefly.sh
+++ scripts/build_firefly.sh
@@ -7,15 +7,35 @@ scripts_dir=$(pwd)/scripts
 echo $rotors_description_dir
 echo $scripts_dir
 
-enable_mavlink_interface=1
-enable_ground_truth=1
-enable_wind=0
-send_vision_estimation=0
-send_odometry=1
+# build non-hil model
+enable_mavlink_interface="true"
+enable_ground_truth="true"
+enable_wind="false"
+send_vision_estimation="false"
+send_odometry="true"
+serial_device='/tmp/ttyACM0'
+serial_enabled="false"
+hil_mode="false"
 
+args="enable_mavlink_interface:=${enable_mavlink_interface} enable_ground_truth:=${enable_ground_truth} enable_wind:=${enable_wind} enable_logging:=${enable_logging} rotors_description_dir:=${rotors_description_dir} send_vision_estimation:=${send_vision_estimation} send_odometry:=${send_odometry} hil_mode:=${hil_mode} serial_device:=${serial_device} serial_enabled:=${serial_enabled}"
 
-python3 ${scripts_dir}/xacro.py -o  ${rotors_description_dir}/urdf/firefly_base.urdf  ${rotors_description_dir}/urdf/firefly_base.xacro enable_mavlink_interface:=${enable_mavlink_interface} enable_ground_truth:=${enable_ground_truth} enable_wind:=${enable_wind} enable_logging:=${enable_logging} rotors_description_dir:=${rotors_description_dir} send_vision_estimation:=${send_vision_estimation} send_odometry:=${send_odometry}
+echo "Args: ${args}"
 
+python3 ${scripts_dir}/xacro.py -o ${rotors_description_dir}/urdf/firefly_base.urdf ${rotors_description_dir}/urdf/firefly_base.xacro ${args}
+
+[[ -e $(pwd)/models/firefly/firefly.sdf ]] && rm $(pwd)/models/firefly/firefly.sdf
 gz sdf -p  ${rotors_description_dir}/urdf/firefly_base.urdf >> $(pwd)/models/firefly/firefly.sdf
+
+rm ${rotors_description_dir}/urdf/firefly_base.urdf
+
+# build HIL model
+serial_device='/tmp/ttyACM0'
+serial_enabled="true"
+hil_mode="true"
+args="enable_mavlink_interface:=${enable_mavlink_interface} enable_ground_truth:=${enable_ground_truth} enable_wind:=${enable_wind} enable_logging:=${enable_logging} rotors_description_dir:=${rotors_description_dir} send_vision_estimation:=${send_vision_estimation} send_odometry:=${send_odometry} hil_mode:=${hil_mode} serial_device:=${serial_device} serial_enabled:=${serial_enabled}"
+python3 ${scripts_dir}/xacro.py -o ${rotors_description_dir}/urdf/firefly_base.urdf ${rotors_description_dir}/urdf/firefly_base.xacro ${args}
+
+[[ -e $(pwd)/models/firefly_hil/firefly_hil.sdf ]] && rm $(pwd)/models/firefly_hil/firefly_hil.sdf
+gz sdf -p  ${rotors_description_dir}/urdf/firefly_base.urdf >> $(pwd)/models/firefly_hil/firefly_hil.sdf
 
 rm ${rotors_description_dir}/urdf/firefly_base.urdf
