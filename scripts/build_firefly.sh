019bc9609e0e35229f4eb268a4e0ad589db96b73
Paul Kremer <p.kremer@uni.lu>
2021-08-31 11:53:18 +0200

    cleanup

--- scripts/build_firefly.sh
+++ scripts/build_firefly.sh
@@ -17,3 +17,5 @@ send_odometry=1
 python3 ${scripts_dir}/xacro.py -o  ${rotors_description_dir}/urdf/firefly_base.urdf  ${rotors_description_dir}/urdf/firefly_base.xacro enable_mavlink_interface:=${enable_mavlink_interface} enable_ground_truth:=${enable_ground_truth} enable_wind:=${enable_wind} enable_logging:=${enable_logging} rotors_description_dir:=${rotors_description_dir} send_vision_estimation:=${send_vision_estimation} send_odometry:=${send_odometry}
 
 gz sdf -p  ${rotors_description_dir}/urdf/firefly_base.urdf >> $(pwd)/models/firefly/firefly.sdf
+
+rm ${rotors_description_dir}/urdf/firefly_base.urdf
